8c9354823f4e10b7526251ff30e9c8fc0b3be23f
mdallen5393 <allen_matthew@outlook.com>
2022-11-23 10:34:53 -0800

    Task 13. HTTP access control (CORS) - updated app.py to allow CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from os import environ
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.register_blueprint(app_views)
 
 
